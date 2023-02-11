dcd6da9794a25c998ef3fdc590303191ba856cfc
Nathan <nathan@naykel.com.au>
2023-02-12 09:39:30 +1000

    add registrations check

--- resources/views/components/dev-toolbar.blade.php
+++ resources/views/components/dev-toolbar.blade.php
@@ -5,29 +5,32 @@
         <div class="container flex space-between va-c">
 
             <div class="flex space-x va-c">
-                <x-gt-sidebar layout="burger-button-main" />
 
-                {{-- @if (Route::has('login-super'))
+                <x-gt-sidebar layout="burger-button-main" class="btn secondary outline" />
 
-                @endif --}}
                 <a href="{{ route('login-super') }}">Login Super</a>
                 <a href="{{ route('login-user') }}">Login User</a>
                 <a href="/admin">Admin</a>
+
                 @if(Route::has('pages.all'))
-                    <a href="{{ route('pages.all') }}">All Pages</a>
+                    <a href="{{ route('pages.all') }}">Site Pages</a>
                 @endif
+
                 @if(Route::has('dev'))
                     <a href="{{ route('dev') }}">Dev</a>
                 @endif
+
             </div>
 
             @auth
                 <x-authit-account-dropdown />
             @else
-                <div>
-                    <a class="hover:txt-secondary mr" href="{{ route('login') }}">Sign In</a>
-                    <a class="hover:txt-secondary " href="{{ route('register') }}">{{ __('Register') }}</a>
-                </div>
+                @if(config('naykel.allow_registrations'))
+                    <div>
+                        <a class="hover:txt-secondary mr" href="{{ route('login') }}">Sign In</a>
+                        <a class="hover:txt-secondary " href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </div>
+                @endif
             @endauth
 
         </div>
